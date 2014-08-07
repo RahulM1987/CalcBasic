601e899306d2839e664e9d7b43af0d54dde897b4
Rahul Morade <rahul@maxdigi.com>
2014-08-07 18:25:03 +0530

    Initial Commit

---
+++ CalculatorBasic/MainAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MainAppDelegate.h
+//  CalculatorBasic
+//
+//  Created by Rahul on 07/08/14.
+//  Copyright (c) 2014 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
